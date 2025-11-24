2ede1f962bb898513cd80187f665ceccad887dbf
GabrielSchauren <gajschauren@minha.fag.edu.br>
2025-11-24 20:21:00 -0300

    Commit 13

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -1,5 +1,11 @@
 public class NewReleasePrice extends Price {
+
    public int getPriceCode() {
       return Movie.NEW_RELEASE;
+   }
+
+   // ✔ cálculo específico de novos lançamentos
+   public double getCharge(int daysRented) {
+      return daysRented * 3;
    }
 }
